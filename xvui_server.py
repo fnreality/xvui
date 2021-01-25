41e396e68d09f993b296a3e94c61b2b803406042
fnreality <landoncol3117@gmail.com>
2021-01-25 11:14:13 -0500

    server

--- xvui_server.py
+++ xvui_server.py
@@ -1,1 +1,26 @@
+import kaon
+from flask import Flask, request
+app = Flask(__name__)
 
+from xv_kaon import XVCtx
+
+@app.route('/')
+def page_root():
+    return ctx.render(file= 'index.kaon.html')
+
+@app.route('/update', methods= ['POST'])
+def page_root_update():
+    with ctx.complete(request.form) as concept:
+        concept.merge([update_params])
+    return ctx.render(file= 'index.kaon.html')
+
+ctx = kaon.Context({
+    'xv': XVCtx()
+})
+
+def update_params(ctx, req):
+    #TODO#
+
+if __name__ == '__main__':
+    ctx.start_reality()
+    app.run(port= 8087)
